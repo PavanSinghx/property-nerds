85e23396e2d32be437288a3717dec51a07edb9ec
Pavan Singh <singhpavan78@gmail.com>
2019-01-10 22:58:51 +0200

    added animations and slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+    let state = 1;
+
+    startBanner();
+
     $(".header-item-left").hover(function () {
 
         $(this).addClass("header-item-active");
@@ -19,5 +23,31 @@ $(document).ready(function () {
         $(this).removeClass("header-item-active");
 
     });
+
+    function startBanner() {
+        $(".call").fadeIn(5000);
+    }
+
+    function open() {
+        $(".header-container").effect("slide", {
+            "direction": "right",
+            "mode": "show"
+        }, 1000, state++);
+    }
+
+    function close(state) {
+
+        $(".header-container").effect("slide", {
+            "direction": "left",
+            "mode": "hide"
+        }, 1000, function () {
+            document.getElementsByClassName("header-container")[0].style.backgroundImage = (state % 2 == 0) ? "url(assets/headerimage.jpg)" : "url(assets/headerimage2.jpg)";
+        });
+    }
+
+    setInterval(() => {
+        close(state);
+        open();
+    }, 5000)
 
 });
